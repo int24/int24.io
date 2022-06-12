57a99f3c5c7998509ef5d5e57a4948af5c16d737
Braden Lamb <braden@int24.io>
2022-06-12 13:20:38 -0400

    feat: analytics

---
+++ src/const/analytics.ts
@@ -0,0 +1,10 @@
+export const GTAG_SRC =
+    'https://www.googletagmanager.com/gtag/js?id=G-4TDB4NLT9H'
+
+export const GA_SCRIPT = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-4TDB4NLT9H');
+`
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
+import Script from 'next/script'
+
 import '@fontsource/space-mono/400.css'
 import '@fontsource/space-mono/700.css'
 
 import '../styles/index.css'
 
+import { GA_SCRIPT, GTAG_SRC } from '../const/analytics'
+
 import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Script id="gtag" src={GTAG_SRC} async />
+            <Script id="ga">{GA_SCRIPT}</Script>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default App
