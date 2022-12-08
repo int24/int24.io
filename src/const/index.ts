ea723f10ebafdcd31e7ce1a2a88cc2edf12b0dc6
braden <braden@retroscepter.com>
2022-12-07 22:42:32 -0500

    Update index.ts

--- src/const/index.ts
+++ src/const/index.ts
@@ -2,4 +2,4 @@ export const DISCORD_USERNAME = 'retro#6666'
 
 export const GITHUB_LINK = 'https://github.com/retroscepter'
 
-export const EMAIL_LINK = 'mailto:braden@retroscepter.io'
+export const EMAIL_LINK = 'mailto:braden@retroscepter.com'
